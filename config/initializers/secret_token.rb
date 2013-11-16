bc13c5827f196c6a504ba8ab4ee72c8977c1b8e3
Alan Turner <a.turner@jaygenius.com>
2013-11-16 17:36:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AloApp::Application.config.secret_token = 'a55d0f4509ceb95e75ce7c16f715a1969daef4cbc97a5436a3caf993701874057597c03967b86524c41ba43fdbf36c3c5c599cebb3ca702800281ada743b647c'
